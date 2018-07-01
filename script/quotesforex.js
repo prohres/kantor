27967c1295741c7e9428e641f827190caffc5dde
Solovii Serhii <prohres@gmail.com>
2018-07-01 21:20:41 +0300

    v.010718

--- script/quotesforex.js
+++ script/quotesforex.js
@@ -1,19 +1,19 @@
 
-var quotes = new XMLHttpRequest();
-quotes.onreadystatechange = function()
+var quotesForex = new XMLHttpRequest();
+quotesForex.onreadystatechange = function()
 {
-  if (quotes.readyState == 4 && quotes.status == 200)
+  if (quotesForex.readyState == 4 && quotesForex.status == 200)
   {
-	var quotesData = JSON.parse(quotes.responseText);
+	var quotesData = JSON.parse(quotesForex.responseText);
 
-	var curEventId = ['eurusd', 'gbpusd', 'usdrub','usdcad', 'usdpln'];
-	for (var i = 0; i < 5; i++) {
-		document.getElementById(curEventId[i] + 'bid').innerHTML = quotesData[i].bid.toFixed(5);
-		document.getElementById(curEventId[i] + 'ask').innerHTML = quotesData[i].ask.toFixed(5);
+	var currencyPairs = ["EURUSD", "GBPUSD", "USDRUB","USDCAD", "USDPLN", "CHFUSD"];
+	for (var i = 0; i < 6; i++) {
+		document.getElementById(currencyPairs[i] + "BID").innerHTML = quotesData[i].bid.toFixed(5);
+		document.getElementById(currencyPairs[i] + "ASK").innerHTML = quotesData[i].ask.toFixed(5);
 	}
   }
 };
-quotes.open("GET", "https://forex.1forge.com/1.0.3/quotes?pairs=EURUSD,GBPUSD,USDRUB,USDCAD,USDPLN&api_key=vEugMMlFdNVX0U5qQuiQ4n8YCUDuEUha", true);
-quotes.send();
+quotesForex.open("GET", "https://forex.1forge.com/1.0.3/quotes?pairs=EURUSD,GBPUSD,USDRUB,USDCAD,USDPLN,CHFUSD&api_key=vEugMMlFdNVX0U5qQuiQ4n8YCUDuEUha", true);
+quotesForex.send();
 
 
---
+++ script/ratekantor.js
@@ -0,0 +1,32 @@
+
+var rateData = [
+	{
+	"cc": "USD",
+	"rateBay": 26.00,
+	"rateSell": 26.13},
+	{
+	"cc": "EUR",
+	"rateBay": 30.30,
+	"rateSell": 30.48},
+	{
+	"cc": "GBP",
+	"rateBay": 34.00,
+	"rateSell": 34.25},
+	{
+	"cc": "RUB",
+	"rateBay": 0.405,
+	"rateSell": 0.410},
+	{
+	"cc": "CAD",
+	"rateBay": 19.40,
+	"rateSell": 19.70},
+	{
+	"cc": "PLN",
+	"rateBay": 6.90,
+	"rateSell": 6.96},
+]
+
+for (var i = 0; i < 6; i++) {
+	document.getElementById(rateData[i]["cc"] + "BAY").innerHTML = rateData[i].rateBay.toFixed(3);
+	document.getElementById(rateData[i]["cc"] + "SELL").innerHTML = rateData[i].rateSell.toFixed(3);
+}
