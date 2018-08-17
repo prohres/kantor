e66be4aebb563e0147f85d2958c784ed451454bf
Solovii Serhii <38884861+prohres@users.noreply.github.com>
2018-08-17 14:37:28 +0300

    v.17.08.18

--- script/request-quotes-rate.js
+++ script/request-quotes-rate.js
@@ -24,34 +24,34 @@ fetch('https://allow-any-origin.appspot.com/https://bank.gov.ua/NBUStatService/v
 
 
 
-// var rateData = [
-// 	{
-// 	"cc": "USD",
-// 	"rateBay": 26.00,
-// 	"rateSell": 26.20},
-// 	{
-// 	"cc": "EUR",
-// 	"rateBay": 30.30,
-// 	"rateSell": 30.48},
-// 	{
-// 	"cc": "GBP",
-// 	"rateBay": 34.00,
-// 	"rateSell": 34.25},
-// 	{
-// 	"cc": "RUB",
-// 	"rateBay": 0.405,
-// 	"rateSell": 0.410},
-// 	{
-// 	"cc": "CAD",
-// 	"rateBay": 19.40,
-// 	"rateSell": 19.70},
-// 	{
-// 	"cc": "PLN",
-// 	"rateBay": 6.90,
-// 	"rateSell": 6.96},
-// ]
+var rateData = [
+	{
+	"cc": "USD",
+	"rateBay": 26.00,
+	"rateSell": 26.20},
+	{
+	"cc": "EUR",
+	"rateBay": 30.30,
+	"rateSell": 30.48},
+	{
+	"cc": "GBP",
+	"rateBay": 34.00,
+	"rateSell": 34.25},
+	{
+	"cc": "RUB",
+	"rateBay": 0.405,
+	"rateSell": 0.410},
+	{
+	"cc": "CAD",
+	"rateBay": 19.40,
+	"rateSell": 19.70},
+	{
+	"cc": "PLN",
+	"rateBay": 6.90,
+	"rateSell": 6.96},
+]
 
-// for (var i = 0; i < 6; i++) {
-// 	document.getElementById(rateData[i]["cc"] + "BAY").innerHTML = rateData[i].rateBay.toFixed(3);
-// 	document.getElementById(rateData[i]["cc"] + "SELL").innerHTML = rateData[i].rateSell.toFixed(3);
-// }
+for (var i = 0; i < 6; i++) {
+	document.getElementById(rateData[i]["cc"] + "BAY").innerHTML = rateData[i].rateBay.toFixed(3);
+	document.getElementById(rateData[i]["cc"] + "SELL").innerHTML = rateData[i].rateSell.toFixed(3);
+}
